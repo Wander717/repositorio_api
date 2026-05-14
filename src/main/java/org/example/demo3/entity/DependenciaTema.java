8b8f527d42193e7ba661340941207befe1c00ae2
Wander717 <wandercpv07@gmail.com>
2026-05-13 21:23:51 -0300

    AAA

---
+++ src/main/java/org/example/demo3/entity/DependenciaTema.java
@@ -0,0 +1,11 @@
+package org.example.demo3.entity;
+
+public class DependenciaTema {
+    private Integer id_dependencia_tema;
+    private Integer tema_id;
+    private Integer tema_dependencia_id;
+    private Integer ordem;
+
+
+
+}
